1293b395ac3c7ccb0e0018a73d069b7f2264c863
prilly <keysaaprillialeddya@gmail.com>
2025-12-16 11:55:34 +0700

    inital dev

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tentang', function () {
+    // ================================================
+    // Route::get() = Tangani HTTP GET request
+    // '/tentang'   = URL yang akan dihandle
+    // function     = Kode yang dijalankan saat URL diakses
+    // ================================================
+
+    return view('tentang');
+    // ↑ return view('tentang') = Tampilkan file tentang.blade.php
+    // ↑ Laravel akan mencari di: resources/views/tentang.blade.php
+});
+
+Route::get('/sapa/{nama?}', function ($nama = "semua") {
+    // ↑ '/sapa/{nama}' = URL pattern
+    // ↑ {nama}         = Parameter dinamis, nilainya dari URL
+    // ↑ function($nama) = Parameter diterima di function
+
+    return "Halo, $nama! Selamat datang di Toko Online.";
+    // ↑ "$nama" = Variable interpolation (masukkan nilai $nama ke string)
+});
